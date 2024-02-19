9bb05355cafccda3c3d96dda4b01c02ee2c506ff
SalsabeelZubaidi <158805325+SalsabeelZubaidi@users.noreply.github.com>
2024-02-20 01:01:43 +0300

    Create Navigation.js

---
+++ src/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavigationBar.css';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="products.js">Products</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
